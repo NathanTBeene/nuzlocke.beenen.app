a1f52ae6d6e6f52fb86d65c9a8b7dbe2adf6ce1b
Bassel T <lessab99@gmail.com>
2024-08-10 13:54:50 -0500

    Updated footer for new site

--- src/lib/components/navs/Footer.svelte
+++ src/lib/components/navs/Footer.svelte
@@ -5,21 +5,21 @@
 
 <footer class={$$restProps.class || ''}>
   <p>
-    <a href="https://www.buymeacoffee.com/nuzlocketracker"
+	  <!--a href="https://www.buymeacoffee.com/nuzlocketracker"
        rel=noreferrer target=_blank>
       <button aria-label='Support me' title='Supprt me'>
         <Icon inline={true} class='fill-current' icon={Coffee} />
       </button>
-    </a>
+    </a-->
 
-    <a href="https://github.com/domtronn/dc-nuzlocke/issues/new?assignees=&labels=&template=bug_report.md&title="
+    <a href="https://github.com/Bassel-T/nuzlocke.app/issues/new?assignees=&labels=&projects=&template=bug_report.md&title="
        rel=noreferrer target=_blank>
       <button aria-label='Report bugs' title='Report bugs'>
         <Icon inline={true} class='fill-current' icon={GitHub} />
       </button>
     </a>
 
-    <a href="https://discord.gg/gtdceegtYB"
+    <a href="https://discord.gg/bmsPuMhmxb"
        rel=noreferrer target=_blank>
       <button aria-label='Discuss & follow new features' title='Discuss & follow new features'>
         <Icon inline={true} class=' fill-current' icon={Discord} />
@@ -30,10 +30,12 @@
 
 
   <span class='inline-flex items-center mb-2'>Made with <Icon inline={true} icon={Heart} class='mx-1 fill-current text-red-500' /> by<strong class=ml-1>Domtronn</strong></span>
+	<br/>
+  <span class='inline-flex items-center mb-2'>Maintained with <Icon inline={true} icon={Heart} class='mx-1 fill-current text-red-500' /> by<strong class=ml-1>Bassel</strong></span>
   <br />
   All content & design © Pokémon Nuzlocke Tracker, 2021-{(new Date()).getFullYear()}. <a href='/privacy-policy'>Privacy Policy</a>. <a href='/credit'>Credits</a>.
   <br class='hidden md:block' />
-  Pokémon images & names © 1995-2021 Nintendo/Creatures Inc./GAME FREAK inc. TM
+  Pokémon images & names © 1995-2024 Nintendo/Creatures Inc./GAME FREAK inc. TM
 </footer>
 
 <!-- svelte-ignore css-unused-selector -->

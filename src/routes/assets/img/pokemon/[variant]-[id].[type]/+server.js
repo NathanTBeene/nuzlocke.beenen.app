1270ee90a3f45afec04ac1ec4dbfde552612fcca
JustAPoring <boesagast@gmail.com>
2025-07-01 04:51:23 +0200

    I really hope this doesn't work

--- src/routes/assets/img/pokemon/[variant]-[id].[type]/+server.js
+++ src/routes/assets/img/pokemon/[variant]-[id].[type]/+server.js
@@ -1,33 +1,24 @@
+
+
 import { Expanded as Games } from '$lib/data/games.js'
 
 
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'node:url';
+let pokemon = import.meta.glob(
+  '/node_modules/pokemon-sprites/sprites/pokemon/*.png'
+)
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const pwd = __dirname.split(path.sep);
 
-function getPath(moduleName, folder = pwd) {
-  if (folder.length < 1) {
-    logError(moduleName, folder);
-    return null;
-  }
-  const nodeModulesPath = folder.concat(["node_modules"]).join(path.sep);
-  const p = moduleName
-    ? path.join(nodeModulesPath, moduleName)
-    : nodeModulesPath;
-  if (fs.existsSync(p)) {
-    return nodeModulesPath;
-  }
-  const res = getPath(moduleName, folder.slice(0, -1));
-  if (!res) logError(moduleName, folder);
-  return res;
+const spriteToPath = async (spriteName) => {
+  let path = `/node_modules/pokemon-sprites/sprites/pokemon/${spriteName}`;
+
+  let test = await pokemon[path]();
+
+  return test;
 }
 
-const spritesPath = `${getPath('pokemon-sprites')}/pokemon-sprites/sprites/pokemon`;
-
+import fs from 'fs';
 export async function GET({ params }) {
+
   const {id, variant} = params;
 
   let spriteName = '';
@@ -41,7 +32,12 @@ export async function GET({ params }) {
       spriteName = `${id}.png`;
   }
 
-  const buffer = fs.readFileSync(`${spritesPath}/${spriteName}`);
+  const sprite = await spriteToPath(spriteName);
 
-  return new Response(buffer);
+  return new Response('', {
+    status: 301,
+    headers: {
+      Location: sprite.default
+    }
+  });
 }

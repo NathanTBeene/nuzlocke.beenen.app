9890513af93a01bf4f8627a2e9fce6bb50f0ec42
JustAPoring <boesagast@gmail.com>
2025-07-01 04:16:41 +0200

    Testing if we can access node_modules in vercel or not

--- src/routes/assets/img/pokemon/[variant]-[id].[type]/+server.js
+++ src/routes/assets/img/pokemon/[variant]-[id].[type]/+server.js
@@ -1,8 +1,32 @@
 import { Expanded as Games } from '$lib/data/games.js'
 
-const spritesPath = `./node_modules/pokemon-sprites/sprites/pokemon`;
 
 import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'node:url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const pwd = __dirname.split(path.sep);
+
+function getPath(moduleName, folder = pwd) {
+  if (folder.length < 1) {
+    logError(moduleName, folder);
+    return null;
+  }
+  const nodeModulesPath = folder.concat(["node_modules"]).join(path.sep);
+  const p = moduleName
+    ? path.join(nodeModulesPath, moduleName)
+    : nodeModulesPath;
+  if (fs.existsSync(p)) {
+    return nodeModulesPath;
+  }
+  const res = getPath(moduleName, folder.slice(0, -1));
+  if (!res) logError(moduleName, folder);
+  return res;
+}
+
+const spritesPath = `${getPath('pokemon-sprites')}/pokemon-sprites/sprites/pokemon`;
+
 export async function GET({ params }) {
   const {id, variant} = params;
 
